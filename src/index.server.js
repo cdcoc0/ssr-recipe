8523b10340d0a8af8839dca34880f26eb7e5b432
velykitty <jiyunee13@gmail.com>
2021-05-21 17:53:16 +0900

    Data loading4 -redux-saga on ssr

--- src/index.server.js
+++ src/index.server.js
@@ -8,8 +8,9 @@ import fs from 'fs';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './modules';
+import rootReducer, {rootSaga} from './modules';
 import PreloadContext from './lib/PreloadContext';
+import createSagaMiddleware, {END} from 'redux-saga';
 
 //asset-manifest.json에서 파일 경로들을 조회
 const manifest = JSON.parse(
@@ -54,7 +55,11 @@ const serverRender = async (req, res, next) => {
     //이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 함
 
     const context = {};
-    const store = createStore(rootReducer, applyMiddleware(thunk));
+    const sagaMiddleware = createSagaMiddleware();
+    const store = createStore(rootReducer, applyMiddleware(thunk, sagaMiddleware));
+
+    const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
+
 
     const preloadContext = {
         done: false,
@@ -72,7 +77,9 @@ const serverRender = async (req, res, next) => {
     );
 
     ReactDOMServer.renderToStaticMarkup(jsx); //renderToStaticMarkup으로 한번 렌더링함
+    store.dispatch(END); //redux-saga의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료됨
     try {
+        await sagaPromise; //기존에 진행 중이던 사가들이 모두 끝날 때까지 기다림
         await Promise.all(preloadContext.promises); //모든 프로미스를 기다림
     } catch(e) {
         return res.status(500);
